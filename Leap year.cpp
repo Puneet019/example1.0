50fd84cd3541bbec2026669e2a10f142d89f32a1
Puneet Dadhich <90840992+Puneet019@users.noreply.github.com>
2021-09-23 14:42:00 +0530

    updated code
     perfect code to check whether  its a leap year or not

--- Leap year.cpp
+++ Leap year.cpp
@@ -1,16 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
-    int a;
-    cin>>a;
+    int year;
 
-    if(a%4 == 0) {
-        cout<<"It's a leap year"<<endl;
+    cout << "Enter a year: ";
+    cin >> year;
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0)
+                cout << year << " is a leap year.";
+            else
+                cout << year << " is not a leap year.";
+        }
+        else
+            cout << year << " is a leap year.";
     }
-    else {
-        cout<<"It's not a leap year"<<endl;
-    }
+    else
+        cout << year << " is not a leap year.";
 
     return 0;
 }
