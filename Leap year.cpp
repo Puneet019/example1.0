e70ba582175973fb17fec4e877602106be4f5474
Manish Ramlani <76808224+error404-png@users.noreply.github.com>
2021-09-23 14:36:45 +0530

    Create Leap year.cpp

---
+++ Leap year.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin>>a;
+
+    if(a%4 == 0) {
+        cout<<"It's a leap year"<<endl;
+    }
+    else {
+        cout<<"It's not a leap year"<<endl;
+    }
+
+    return 0;
+}
