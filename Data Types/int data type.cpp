eae544568ea7ff22b64c414691a2969281fd9e38
Puneet Dadhich <90840992+Puneet019@users.noreply.github.com>
2021-10-15 11:36:37 +0530

    code updated  in  int data type.cpp

---
+++ Data Types/int data type.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){                                                  // code for  basic Data Types
+
+    
+ int a;
+ long long int b;
+ char c;
+ float d;
+ double e;
+
+ scanf("%d %lld %c %f %lf",&a,&b,&c,&d,&e);
+
+ printf("%d\n%lld\n%c\n%f\n%lf",a,b,c,d,e);
+
+return 0;
+}
